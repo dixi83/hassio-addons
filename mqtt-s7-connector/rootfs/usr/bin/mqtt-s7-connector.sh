f90cec15d5dee823b7d37fd537ae42087a493bca
dixi83 <martijn.diks@gmail.com>
2024-06-12 18:05:57 +0000

    debugging

--- mqtt-s7-connector/rootfs/usr/bin/mqtt-s7-connector.sh
+++ mqtt-s7-connector/rootfs/usr/bin/mqtt-s7-connector.sh
@@ -4,10 +4,10 @@
 # This file is in etc/s6-overlay/s6-rc.d/mqtt-s7-connector/run
 # -------------------------------------------------------------
 
-echo $(bashio::config)
+bashio::log.debug $(bashio::config)
 
-declare loglevel
 declare -a command
+declare loglevel
 declare log_level
 
 log_level=$(bashio::string.lower "$(bashio::config log_level invalid)")
@@ -18,7 +18,7 @@ if [ "$log_level" = "invalid" ]; then
 fi
 
 # set log level >> 0: Trace, 1: Debug, 2: Info, 3: Notice, 4: Warning, 5: Error, 6: Fatal'
-case log_level in
+case bashio::config log_level in
   'trace')
     loglevel='0'
     ;;
@@ -45,6 +45,8 @@ case log_level in
     bashio::log.warning 'Unknown log level has been set, took 4 to continue the startup'
     ;;
 esac
+bashio::log.debug 'got log_level:'
+bashio::log.debug $(loglevel)
 
 if bashio::config.has_value 'config_files'; then 
   command=''
