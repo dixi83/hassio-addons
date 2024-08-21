5c84846de940c94057096dbbca68815bc1a0b1d9
dixi83 <martijn.diks@gmail.com>
2024-08-21 11:08:59 +0000

    some fixes

--- ha-mbmd/rootfs/usr/bin/ha-mbmd.sh
+++ ha-mbmd/rootfs/usr/bin/ha-mbmd.sh
@@ -5,46 +5,6 @@
 # Runs the ha-mbmd
 # ==============================================================================
 declare command
-declare loglevel
-declare log_level
-declare first
-
-log_level=$(bashio::string.lower "$(bashio::config log_level invalid)")
-
-if [ "$log_level" = "invalid" ]; then
-  bashio::log.magenta 'Received invalid log_level from config, fallback to warning'
-  log_level="warning"
-  loglevel=4
-fi
-
-# set log level >> 0: Trace, 1: Debug, 2: Info, 3: Notice, 4: Warning, 5: Error, 6: Fatal'
-case "$log_level" in
-  "trace")
-    loglevel="0"
-    ;;
-  "debug")
-    loglevel="1"
-    ;;
-  "info")
-    loglevel="2"
-    ;;
-  "notice")
-    loglevel="3"
-    ;;
-  "warning")
-    loglevel="4"
-    ;;
-  "error")
-    loglevel="5"
-    ;;
-  "fatal")
-    loglevel="6"
-    ;;
-  *)
-    loglevel="4"
-    bashio::log.red 'Unknown log level has been set, took 4 to continue the startup'
-    ;;
-esac
 
 command="/usr/local/bin/mbmd run"
 
